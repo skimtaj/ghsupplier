8c089cfd6d652947fb0e46457a33ef26c3997bc3
Sk <skimtaj779@gmail.com>
2026-09-03 17:46:11 +0530

    Admin dashboard chnages

--- adminModule/controllers/admin_controller.js
+++ adminModule/controllers/admin_controller.js
@@ -134,7 +134,7 @@ const adminDashboard = async (req, res) => {
         }
 
         const page = Number(req.query.page) || 1;
-        const limit = 2;
+        const limit = 10;
         const skip = (page - 1) * limit;
         const serialNo = skip;
 
